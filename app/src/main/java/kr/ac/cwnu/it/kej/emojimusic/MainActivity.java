7f934a57c0626b09e6c402e0436f6d42ce8f40e9
김은지 <euni@gim-eunjiui-MacBookAir.local>
2021-12-03 17:06:23 +0900

    Emoji early ver

---
+++ app/src/main/java/kr/ac/cwnu/it/kej/emojimusic/MainActivity.java
@@ -0,0 +1,74 @@
+
+package kr.ac.cwnu.it.kej.emojimusic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn_move;
+    private Button btn_move1;
+    private Button btn_move2;
+    private Button btn_move3;
+    private ImageView img;
+    //private String str;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_move = findViewById(R.id.btn_move);
+        btn_move.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SubActivity.class);
+                //intent.putExtra("str", str);
+                startActivity(intent);  // 액티비티 이동
+            }
+        });
+
+        btn_move1 = findViewById(R.id.btn_move1);
+        btn_move1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SubActivity1.class);
+                startActivity(intent);  // 액티비티 이동
+            }
+        });
+
+        btn_move2 = findViewById(R.id.btn_move2);
+        btn_move2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SubActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_move3 = findViewById(R.id.btn_move3);
+        btn_move3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SubActivity3.class);
+                startActivity(intent);
+            }
+        });
+
+        img = (ImageView) findViewById(R.id.img);
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "랜덤 기분 넣을 예정", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
